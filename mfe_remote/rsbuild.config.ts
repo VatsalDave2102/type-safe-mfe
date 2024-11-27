44084ec17c935bef402436bfbf41b9a25b375500
Vatsal Dave <vatsaldave2002@gmail.com>
2024-11-27 11:11:52 +0530

    update cors config

--- mfe_remote/rsbuild.config.ts
+++ mfe_remote/rsbuild.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+      'Access-Control-Allow-Headers': '*',
     },
   },
 });
