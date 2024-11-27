3b6d304c7aa05f72f2c4d35decc3fcf825b1b7b1
Vatsal Dave <vatsaldave2002@gmail.com>
2024-11-27 11:02:58 +0530

    add cors config in host

--- mfe_host/rsbuild.config.ts
+++ mfe_host/rsbuild.config.ts
@@ -15,5 +15,10 @@ export default defineConfig({
   ],
   server: {
     port: 3001,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+    },
   },
 });
